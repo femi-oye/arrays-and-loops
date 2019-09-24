295954ccb7f28869eb88a0abbaf0a6b111ac4421
femioyedepo <femoye@gmail.com>
2019-09-23 20:20:12 -0500

    Completed Assignment 1A - basic for loop

--- arraysAndLoops.js
+++ arraysAndLoops.js
@@ -1,2 +1,19 @@
-let hello = "hello world";
-    console.log(hello)
+// Declare two empty arrays named: days_of_the_week and my_schedule
+let days_of_the_week = [];
+let my_schedule = [];
+
+// Push the days of the week into days_of_the_week. Fill the other array with your work schedule.
+days_of_the_week.push('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+console.log(days_of_the_week.length);
+console.table(days_of_the_week);
+
+
+my_schedule.push('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+console.log(my_schedule.length);
+console.table(my_schedule);
+
+// Assignment 1A - for loop
+for (i = 0; i < my_schedule.length ; i++) {
+    console.log(my_schedule[i])
+
+}
